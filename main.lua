4528143c561fc1ab52c250d641abea6dd5f11f22
thepaddy6456464 <118495606+thepaddy6456464@users.noreply.github.com>
2025-08-14 19:26:18 +1000

    Update main.lua
    test for doors door esp

--- main.lua
+++ main.lua
@@ -1,89 +1,93 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
-ESP.Players = true
-ESP.Boxes = true
-ESP.Names = true
-ESP:Toggle(true)
+local Options = {
+    ESPS_Font = { Value = Enum.Font.SourceSans },
+    ESPS_FontSize = { Value = 20 },
+    ESPS_FillTransparency = { Value = 0.7 },
+    ESPS_OutlineTransparency = { Value = 0.2 },
+    ESPS_FadeTime = { Value = 1 },
+}
+local Library = { Unloaded = false }
+local EspTable = { Interactables = { Doors = {} } }
 
-local Window = Rayfield:CreateWindow({
-   Name = "Russainhub",
-   Icon = 0, 
-   LoadingTitle = "warning:detection status is unknown",
-   LoadingSubtitle = "by pulse",
-   ShowText = "russain is very pregnant", 
-   Theme = "Default", 
+local function Esp(Parent, TextAdornee, Text, Color, OutlineColor)
+    local BillboardGui = Instance.new("BillboardGui", Parent)
+    local TextLabel = Instance.new("TextLabel", BillboardGui)
+    local Highlight = Instance.new("Highlight", Parent)
 
-   ToggleUIKeybind = "K", 
+    BillboardGui.Adornee = TextAdornee
+    BillboardGui.AlwaysOnTop = true
+    BillboardGui.Size = UDim2.fromScale(1, 1)
 
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, 
+    TextLabel.Size = UDim2.fromScale(1, 1)
+    TextLabel.TextStrokeTransparency = 0
+    TextLabel.Font = Options.ESPS_Font.Value
+    TextLabel.TextSize = Options.ESPS_FontSize.Value
+    TextLabel.TextColor3 = Color
+    TextLabel.BackgroundTransparency = 1
 
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil,
-      FileName = "Big Hub"
-   },
+    Highlight.Adornee = Parent
+    Highlight.FillColor = Color
+    Highlight.OutlineColor = OutlineColor or Color
+    TextLabel.TextTransparency = 1
+    Highlight.FillTransparency = 1
+    Highlight.OutlineTransparency = 1
 
-   Discord = {
-      Enabled = false, 
-      Invite = "noinvitelink", 
-      RememberJoins = true 
-   },
+    TextLabel:SetAttribute("Text", Text)
 
-   KeySystem = false, 
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", 
-      FileName = "Key", 
-      SaveKey = true, 
-      GrabKeyFromSite = false, 
-      Key = {"Hello"} 
-   }
-})
+    task.spawn(function()
+        while Parent and not Library.Unloaded do
+            task.wait()
+            local dist = (workspace.CurrentCamera.CFrame.Position - Parent:GetPivot().Position).Magnitude
+            TextLabel.Text = Text .. "\n[ " .. (dist <= 9.9 and string.format("%.1f", dist) or string.format("%.0f", dist)) .. " ]"
+        end
+    end)
 
-Rayfield:Notify({
-   Title = "RussainHub",
-   Content = "Russainspy is very pregnant",
-   Duration = 6.5,
-   Image = 4483362458,
-})
+    local ts = game:GetService("TweenService")
+    ts:Create(Highlight, TweenInfo.new(Options.ESPS_FadeTime.Value), {
+        FillTransparency = Options.ESPS_FillTransparency.Value
+    }):Play()
+    ts:Create(Highlight, TweenInfo.new(Options.ESPS_FadeTime.Value), {
+        OutlineTransparency = Options.ESPS_OutlineTransparency.Value
+    }):Play()
+    ts:Create(TextLabel, TweenInfo.new(Options.ESPS_FadeTime.Value), {
+        TextTransparency = 0
+    }):Play()
 
-local MainTab = Window:CreateTab("main", 4483362458)
+    return Highlight, TextLabel
+end
 
-local MainButton = MainTab:CreateButton({
-   Name = "esp prototype",
-   Callback = function()
-   -- loadstring
-   wait(1)
 
--- config
+local function RunDoorESP()
+   
+    for i, v in ipairs(EspTable.Interactables.Doors) do
+        if v[1] and v[1].Parent then
+            RemoveEsp(v[1].Parent)
+        end
+    end
+    EspTable.Interactables.Doors = {}
 
- 
--- object
-ESP:AddObjectListener(Workspace, { game:GetService("Workspace").Skyscraper.Base
-    Name = "weirdo", --Object name inside of the path, for example: Workspace.ThisFolder.Item_1
-    CustomName = "", -- Name you want to be displayed
-    Color = Color3.fromRGB(0, 0, 0), -- Color
-    IsEnabled = "whatever" -- Any name, has to be the same as the last line: ESP.TheNameYouWant
-})
-ESP.whatever = true
-   end,
-})
 
-local WalkspeedSlider = MainTab:CreateSlider({
-   Name = "speed",
-   Range = {0, 300},
-   Increment = 1,
-   Suffix = "Walkspeed",
-   CurrentValue = 16,
-   Flag = "Walkspeed", 
-   Callback = function(Value)
-   local player = game.Players.LocalPlayer
-            if player and player.Character and player.Character:FindFirstChild("Humanoid") then
-                player.Character.Humanoid.WalkSpeed = Value
+    local Rooms = workspace.CurrentRooms
+    for _, room in pairs(Rooms:GetChildren()) do
+        if room:IsA("Model") and room:FindFirstChild("Door") then
+            local door = room:FindFirstChild("Door")
+            local part = door.PrimaryPart or door:FindFirstChildWhichIsA("BasePart")
+            if part then
+                local h, t = Esp(
+                    door,
+                    part,
+                    "Door",
+                    Color3.new(1, 1, 1),
+                    Color3.new(1, 1, 1)
+                )
+                table.insert(EspTable.Interactables.Doors, {h, t})
             end
-   end,
-})
+        end
+    end
+end
 
 
+game:GetService("RunService").RenderStepped:Connect(function()
+    if not Library.Unloaded then
+        RunDoorESP()
+    end
+end)
